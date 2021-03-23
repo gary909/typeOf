71f6c0bc9c1f7a4dd069ec43be7789ae3584a2f8
gary <gary.white@lodgit.com>
2021-03-23 16:40:49 +0100

    code

--- magic.js
+++ magic.js
@@ -8,6 +8,13 @@ var test7 = {"abcdef": 123};
 var test8 = ["abcdef", 123];
 function test9() {return "abcdef"};
 var test10 = null;
+var test11 = 0 / 0;
+
+if (99 == "99") {
+    console.log("A number equals a string!");
+} else {
+    console.log("No way a number equals a string");
+}
 
 console.log(typeof test1);
 console.log(typeof test2);
@@ -19,3 +26,28 @@ console.log(typeof test7);
 console.log(typeof test8);
 console.log(typeof test9);
 console.log(typeof test10);
+console.log(typeof test11);
+
+var text = "YOU SHOULD NEVER SHOUT WHEN TYPING";
+var presentableText = text.toLowerCase();
+if (presentableText.length > 0) {
+    alert(presentableText);
+}
+
+var emot = "X0xx00";
+var hugs = 0;
+var kisses = 0;
+
+emot = emot.trim();
+emot = emot.toUpperCase();
+
+for(var i = 0; i < emot.length; i++) {
+    if (emot.charAt(i) === "X") {
+        hugs++;
+    } else if (emot.charAt(i) == "0") {
+        kisses++;
+    }
+}
+
+console.log("hugs: " + hugs + " Kisses: " + kisses);
+console.log("That's your lot");
